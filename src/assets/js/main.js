d6a67f3d9600e525cfae99a20740c43c06032572
Klimovich Yauhenia <zh.shuravina@gmail.com>
2022-01-18 11:07:58 +0300

    footer structure and style commit

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,3 +1,14 @@
-console.log(test(5, 2));
-console.log(test(2, 4));
-console.log(test(9, 1));
+// console.log(test(5, 2));
+
+function burgerMenu() {
+    let burgerIcon = document.querySelector(".icon-menu");
+    let closeIcon = document.getElementById("close_icon");
+    let burgerMenu = document.getElementById("burger__menu");
+
+    burgerIcon.addEventListener("click", function() {
+        burgerMenu.classList.toggle("_active")
+        // .icon-menu.
+    })
+}
+
+burgerMenu()
